2a395455577c374ce8c3aec256a06859c5ad50a3
yin <yin@swifthealth.cn>
2018-05-16 18:28:22 +0800

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const bankLogo = require('./bankLogo.json')
 
 export default function getBankLogoSpell(bankChineseName) {
-  return bankLogo.find(({name}) => name === bankChineseName)
+  return bankLogo.find(({name}) => name.indexOf(bankChineseName) !== -1)
 }
